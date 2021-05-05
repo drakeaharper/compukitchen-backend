9de75e2cead9982ee71975e2056531dcf719781b
Drake Harper <drakeaharper@gmail.com>
2021-05-05 14:18:44 -0600

    forgot to build last changes to prod server

--- prod-server/api/submission/submission-routes.js
+++ prod-server/api/submission/submission-routes.js
@@ -9,6 +9,8 @@ var _express = _interopRequireDefault(require("express"));
 
 var controller = _interopRequireWildcard(require("./submission-controller"));
 
+var auth = _interopRequireWildcard(require("../../services/auth-service"));
+
 function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function () { return cache; }; return cache; }
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
@@ -17,10 +19,10 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 const router = _express.default.Router();
 
-router.post('/user_submit', controller.userSubmit);
-router.get('/user_submissions/:id', controller.userSubmissions);
-router.get('/submissions_to_process', controller.submissionsToProcess);
-router.put('/process/:id', controller.process);
-router.get('/past_submissions', controller.pastSubmissions);
+router.post('/user_submit', auth.requireLogin, controller.userSubmit);
+router.get('/user_submissions/:id', auth.requireLogin, controller.userSubmissions);
+router.get('/submissions_to_process', auth.requireLogin, controller.submissionsToProcess);
+router.put('/process/:id', auth.requireLogin, controller.process);
+router.get('/past_submissions', auth.requireLogin, controller.pastSubmissions);
 var _default = router;
 exports.default = _default;
--- prod-server/api/user/user-routes.js
+++ prod-server/api/user/user-routes.js
@@ -9,6 +9,8 @@ var _express = _interopRequireDefault(require("express"));
 
 var controller = _interopRequireWildcard(require("./user-controller"));
 
+var auth = _interopRequireWildcard(require("../../services/auth-service"));
+
 function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function () { return cache; }; return cache; }
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
@@ -17,8 +19,8 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 const router = _express.default.Router();
 
-router.get('/user', controller.index);
-router.get('/user/:id', controller.show);
-router.put('/user/:id', controller.update);
+router.get('/user', auth.requireLogin, controller.index);
+router.get('/user/:id', auth.requireLogin, controller.show);
+router.put('/user/:id', auth.requireLogin, controller.update);
 var _default = router;
 exports.default = _default;
